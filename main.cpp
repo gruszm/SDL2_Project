d31e0f859bb60aa99e71e7d726d91eadb6647e77
MariuszGruszczynski <78258950+MariuszGruszczynski@users.noreply.github.com>
2021-09-16 17:01:52 +0200

    Init, load functions
    Added Init(), loadMedia(), loadBMP(), close() funcs

--- main.cpp
+++ main.cpp
@@ -1,30 +1,70 @@
 #include <stdio.h>
+#include <string>
 #include <SDL.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+#define WINDOW_TITLE "WINDOW"
 
 using namespace std;
 
-int main(int argc, char **argv) {
+SDL_Window *window = NULL;
+SDL_Surface *screen = NULL;
+SDL_Surface *bmp = NULL;
+
+bool loadBMP(SDL_Surface *&surface, string path) {
+    surface = SDL_LoadBMP(path.c_str());
+    if(!surface) {
+        printf(SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+bool init() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Initialization error: %s", SDL_GetError());
-        return 0;
+        return false;
     }
 
-    SDL_Window *window = SDL_CreateWindow("WINDOW", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALWAYS_ON_TOP);
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if(!window) {
-        printf("window error");
+        printf("Window could not be created: %s", SDL_GetError());
         SDL_Quit();
-        return 0;
+        return false;
     }
+    screen = SDL_GetWindowSurface(window);
 
-    SDL_Surface *surf = SDL_GetWindowSurface(window);
-    if(!surf) {
-        printf("surface error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 0;
+    return true;
+}
+
+void close() {
+    SDL_FreeSurface(bmp);
+    bmp = NULL;
+
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    SDL_Quit();
+}
+
+bool loadMedia() {
+    bool is_good = true;
+
+    if(!loadBMP(bmp, "alamakota.bmp"))
+        is_good = false;
+
+    return is_good;
+}
+
+int main(int argc, char **argv) {
+    if(!init())
+        return -1;
+
+    if(!loadMedia()) {
+        close();
+        return -2;
     }
 
     SDL_Event e;
@@ -47,14 +87,14 @@ int main(int argc, char **argv) {
             colorShift -= 50;
         if(keys[SDL_SCANCODE_DOWN])
             colorShift += 90;
+        if(keys[SDL_SCANCODE_ESCAPE])
+            quit = true;
 
-        SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 70 + colorShift, 120 + colorShift, 50 + colorShift));
+        SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 80 + colorShift, 180 - colorShift, 130 + colorShift));
 
         SDL_UpdateWindowSurface(window);
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    close();
     return 0;
 }
